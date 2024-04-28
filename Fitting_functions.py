69cb04006401e107d2016d1663b344bbfdd3814d
18rway <18rway@berkeley.edu>
2024-04-28 12:55:30 -0400

    Initial development files

--- Fitting_functions.py
+++ Fitting_functions.py
@@ -6,74 +6,71 @@ import Reading_in as r
 import emcee
 import corner
 import warnings
+import argparse
 warnings.filterwarnings("ignore")
-filename=sys.argv[1]
+parser = argparse.ArgumentParser(description='Fit spectral lines')
+parser.add_argument('filename', help='filename of spectrum that you want to fit')
+#Default values
+parser.add_argument('-z', '--redshift', default=0, type=float,
+                    help='redshift of object to be used to correct wavelengths to rest wavelengths')
+parser.add_argument('--correction', default=0,type=float,
+                    help='Dust correction term')
+parser.add_argument('--pm', default=0,type=float,
+                    help='Range from rest wavelength over which to perform fitting')
+parser.add_argument('--wavelength', default=0,type=float,
+                    help='Rest wavelength of line')
+parser.add_argument('--guess',help='Best guess file')
+args = parser.parse_args()
+filename=args.filename
+redshift=args.redshift
+lambda_pm=args.pm
+guess=np.loadtxt(args.guess)
 x,y,yerr=r.get_wavelength_vals(filename)
-def model_4gauss(theta):
-    a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12 = theta
-    model = a1*np.exp(-(x-a2)**2/(2*a3**2))+a4*np.exp(-(x-a5)**2/(2*a6**2))+a7*np.exp(-(x-a8)**2/(2*a9**2))+a10*np.exp(-(x-a11)**2/(2*a12**2))
-    return model
-def model_3gauss(theta):
-    a1,a2,a3,a4,a5,a6,a7,a8,a9 = theta
-    model = a1*np.exp(-(x-a2)**2/(2*a3**2))+a4*np.exp(-(x-a5)**2/(2*a6**2))+a7*np.exp(-(x-a8)**2/(2*a9**2))
-    return model
-def model_2gauss(theta):
-    a1,a2,a3,a4,a5,a6= theta
-    model = a1*np.exp(-(x-a2)**2/(2*a3**2))+a4*np.exp(-(x-a5)**2/(2*a6**2))
-    return model
-def model_1gauss(theta):
-    a1,a2,a3 = theta
-    model = a1*np.exp(-(x-a2)**2/(2*a3**2))
-    return model
-def model_lorentzian(theta):
-    a,b,c=theta
-    model=(a/np.pi)*(c/((x-b)**2+c**2))
-    return model
-def three_gaussian_fit( params ):
-    fit = model_3gauss( x, params )
-    return (fit - y)
-def two_gaussian_fit( params ):
-    fit = model_2gauss( x, params )
-    return (fit - y)
-def one_gaussian_fit( params ):
-    fit =model_1gauss( x, params )
-    return (fit - y)
-def four_gaussian_fit( params ):
-    fit = model_4gauss( x, params )
-    return (fit - y)
-def lorentzian_fit(params):
-    fit = model_lorentzian(params)
-    return (fit - y)
+if len(guess)==6:
+    model='2g'
+elif len(guess)==9:
+    model='3g'
+elif len(guess)==12:
+    model='4g'
+elif len(guess)==3 and sys.argv[7]!='L':
+    model='1g'
+else:
+    model='l'
 def lnlike(theta, x, y, yerr):
         if model=='1g':
-            lnl=-np.sum((y-model_1gauss(theta))**2/yerr**2)/2
+            lnl=-np.sum((y-r.model_1gauss(theta))**2/yerr**2)/2
         elif model=='2g':
-            lnl=-np.sum((y-model_2gauss(theta))**2/yerr**2)/2
+            lnl=-np.sum((y-r.model_2gauss(theta))**2/yerr**2)/2
         elif model=='3g':
-            lnl=-np.sum((y-model_3gauss(theta))**2/yerr**2)/2
+            lnl=-np.sum((y-r.model_3gauss(theta))**2/yerr**2)/2
         elif model=='4g':
-            lnl=-np.sum((y-model_4gauss(theta))**2/yerr**2)/2
+            lnl=-np.sum((y-r.model_4gauss(theta))**2/yerr**2)/2
         return lnl
 def log_prior(theta):
         if model=='2g':
             a,b,c,d,e,f= theta
-            if 0<a<5 and -500<b<500 and -1000<c<1000 and 0<d<5 and -1000<e<1000 and -1500<f<1500:
+            a0,b0,c0,d0,e0,f0=abs(guess)
+            if -4*a0<a<4*a0 and -4*b0<b<4*b0 and -4*c0<c<4*c0 and -4*d0<d<4*d0 and -4*e0<e<4*e0 and -4*f0<f<4*f0:
                 return 0.0
-        if model=='1g':
+        elif model=='1g':
             a,b,c= theta
-            if 0<a<5 and -500<b<500 and -1000<c<1000:
+            a0,b0,c0=guess
+            if -4*abs(a0)<a<4*abs(a0) and -4*abs(b0)<b<4*abs(b0) and -4*abs(c0)<c<4*abs(c0):
                 return 0.0
-        if model=='l':
+        elif model=='l':
             a,b,c= theta
+            a0,b0,c0=guess
             if 0<a<5 and -500<b<500 and -10000<c<10000:
                 return 0.0
-        if model=='3g':
+        elif model=='3g':
             a,b,c,d,e,f,g,h,i= theta
-            if 0<a<5 and -500<b<500 and -1000<c<1000 and 0<d<5 and -1000<e<1000 and -1500<f<1500 and 0<g<5 and -1500<h<1500 and -1e4<i<1e4:
+            a0,b0,c0,d0,e0,f0,g0,h0,i0= guess
+            if--4*a0<a<4*a0 and -4*b0<b<4*b0 and -4*c0<c<4*c0 and -4*d0<d<4*d0 and -4*e0<e<4*e0 and -4*f0<f<4*f0 and -4*g0<g<4*g0 and -4*h0<h<4*h0 and -4*i0<i<4*i0:
                 return 0.0
-        if model=='4g':
+        elif model=='4g':
             a,b,c,d,e,f,g,h,i,j,k,l= theta
-            if 0<a<5 and -10000<b<10000 and -10000<c<10000 and 0<d<5 and -500<e<500 and -1500<f<1500 and 0<g<5 and -1000<h<1000 and -5e2<i<5e2 and 0<j<5 and -2e3<k<2e3 and -5e2<l<5e2:
+            a0,b0,c0,d0,e0,f0,g0,h0,i0,j0,k0,l0=abs(guess)
+            if -4*a0<a<4*a0 and -4*b0<b<4*b0 and -4*c0<c<4*c0 and -4*d0<d<4*d0 and -4*e0<e<4*e0 and -4*f0<f<4*f0 and -4*g0<g<4*g0 and -4*h0<h<4*h0 and -4*i0<i<4*i0 and -4*j0<j<4*j0 and -4*k0<k<4*k0 and -4*l0<l<4*l0:  
                 return 0.0
         return -np.inf
 def lnprob(theta, x,y,yerr):
@@ -92,50 +89,154 @@ def main(p0,nwalkers,niter,ndim,lnprob,data):
         pos, prob, state = sampler.run_mcmc(p0, niter)
 
         return sampler, pos, prob, state
-model=sys.argv[6]
-def run_chains(model):
-    data = (x,y,yerr)
-    nwalkers=200
-    niter=10000
-    if model=='1g':
-        initial = np.array([0.1,0,100] )
-    elif model=='l':
-        initial = np.array([0.3,0,1000] )
-    elif model=='2g':
-        initial = np.array([0.1,0,100,0.3,0,1000] )
-    elif model=='3g':
-        initial = np.array([1.5,-50,2000,0.2,-500,1000,0.8,0,50] )
-    elif model=='4g':
-        initial=[0.3,-5000,5000,0.8,100,950,0.3,50,100,0.2,1000,240]
-    ndim=len(initial)
-    p0 = [np.array(initial) + 1e-7 * np.random.randn(ndim) for i in range(nwalkers)]
-    sampler, pos, prob, state = main(p0,nwalkers,niter,ndim,lnprob,data)
-    samples=sampler.flatchain
-    theta=samples[np.argmax(sampler.flatlnprobability)]
-    labels=['a','b','c','d','e','f','g','h','i','j','k','l']
-    corner.corner(samples,show_titles=True,labels=labels,plot_datapoints=True,quantiles=[0.16, 0.5, 0.84],title_fmt='.3f')
-    plt.savefig('MCMC.png',format='png')
-    plt.show()
-    return theta
+data = (x,y,yerr)
+nwalkers=200
+niter=10000
+ndim=len(guess)
+p0 = [np.array(guess) + 1e-7 * np.random.randn(ndim) for i in range(nwalkers)]
+sampler, pos, prob, state = main(p0,nwalkers,niter,ndim,lnprob,data)
+samples=sampler.flatchain
+tau = sampler.get_autocorr_time()
+for i in range(len(tau)):
+    if 40*tau[i]<niter:
+        print('The chains converged for parameter {}'.format{i})
+    else:
+        print('Convergence failed')
+theta=samples[np.argmax(sampler.flatlnprobability)]
+labels=['a','b','c','d','e','f','g','h','i','j','k','l']
+corner.corner(samples,show_titles=True,labels=labels,plot_datapoints=True,quantiles=[0.16, 0.5, 0.84],title_fmt='.3f')
+plt.savefig('MCMC.png',format='png')
+plt.show()
 def plotting(model):
-    theta=run_chains(model)
     if model=='2g':
-        plt.plot(x,model_2gauss(theta),color='blue')
-        print('MCMC reduced chi squared=',1/(len(x)-len(theta))*sum((y-model_2gauss(theta))**2/(yerr**2)))
+        plt.plot(x,r.model_2gauss(theta),color='blue')
+        #print('MCMC reduced chi squared=',1/(len(x)-len(theta))*sum((y-model_2gauss(theta))**2/(yerr**2)))
     elif model=='3g':
-        plt.plot(x,model_3gauss(theta),color='blue')
-        print('MCMC reduced chi squared=',1/(len(x)-len(theta))*sum((y-model_3gauss(theta))**2/(yerr**2)))
+        plt.plot(x,r.model_3gauss(theta),color='blue')
+        #print('MCMC reduced chi squared=',1/(len(x)-len(theta))*sum((y-model_3gauss(theta))**2/(yerr**2)))
     elif model=='1g':
-        plt.plot(x,model_1gauss(theta),color='blue')
-        print('MCMC reduced chi squared=',1/(len(x)-len(theta))*sum((y-model_1gauss(theta))**2/(yerr**2)))
+        plt.plot(x,r.model_1gauss(theta),color='blue')
+        #print('MCMC reduced chi squared=',1/(len(x)-len(theta))*sum((y-model_1gauss(theta))**2/(yerr**2)))
     elif model=='4g':
-        plt.plot(x,model_4gauss(theta),color='blue')
-        print('MCMC reduced chi squared=',1/(len(x)-len(theta))*sum((y-model_4gauss(theta))**2/(yerr**2)))
+        plt.plot(x,r.model_4gauss(theta),color='blue')
+        #print('MCMC reduced chi squared=',1/(len(x)-len(theta))*sum((y-model_4gauss(theta))**2/(yerr**2)))
     elif model=='l':
-        plt.plot(x,model_lorentizan(theta),color='blue')
-        print('MCMC reduced chi squared=',1/(len(x)-len(theta))*sum((y-model_lorentzian(theta))**2/(yerr**2)))
-    plt.plot(x,y,color='black')
-    plt.xlim(-10000,10000)
+        plt.plot(x,r.model_lorentizan(theta),color='blue')
+        #print('MCMC reduced chi squared=',1/(len(x)-len(theta))*sum((y-model_lorentzian(theta))**2/(yerr**2)))
+    plt.plot(x,y,color='black',alpha=0.5)
+    #plt.xlim(-10000,10000)
     plt.savefig('Fitted.png')
     plt.show()
+if model=='4g':
+    flat_samples = sampler.get_chain(discard=100, thin=15, flat=True)
+    df=pd.DataFrame(columns=['Comp1_Amp','Comp1_Center','Comp1_SD','Comp2_Amp','Comp2_Center','Comp2_SD','Comp3_Amp','Comp3_Center','Comp3_SD','Comp4_Amp','Comp4_Center','Comp4_SD'])
+    for i in range(ndim):
+        mcmc = np.percentile(flat_samples[:, i], [16, 50, 84])
+        q = np.diff(mcmc)
+        txt = "{0:.3f}_{{-{1:.3f}}}^{{{2:.3f}}}"
+        txt = txt.format(mcmc[1], q[0], q[1], labels[i])
+        if i==0:
+            df['Comp1_Amp']=[txt]
+        if i==1:
+            df['Comp1_Center']=[txt]
+        if i==2:
+            df['Comp1_SD']=[txt]
+        if i==3:
+            df['Comp2_Amp']=[txt]
+        if i==4:
+            df['Comp2_Center']=[txt]
+        if i==5:
+            df['Comp2_SD']=[txt]
+        if i==6:
+            df['Comp3_Amp']=[txt]
+        if i==7:
+            df['Comp3_Center']=[txt]
+        if i==8:
+            df['Comp3_SD']=[txt]
+        if i==9:
+            df['Comp4_Amp']=[txt]
+        if i==10:
+            df['Comp4_Center']=[txt]
+        if i==11:
+            df['Comp4_SD']=[txt]
+    df.to_csv('Final Results.csv')
+if model=='3g':
+    flat_samples = sampler.get_chain(discard=100, thin=15, flat=True)
+    df=pd.DataFrame(columns=['Comp1_Amp','Comp1_Center','Comp1_SD','Comp2_Amp','Comp2_Center','Comp2_SD','Comp3_Amp','Comp3_Center','Comp3_SD','Comp4_Amp','Comp4_Center','Comp4_SD'])
+    ndim=len(guess)
+    for i in range(ndim):
+        mcmc = np.percentile(flat_samples[:, i], [16, 50, 84])
+        q = np.diff(mcmc)
+        txt = "{0:.3f}_{{-{1:.3f}}}^{{{2:.3f}}}"
+        txt = txt.format(mcmc[1], q[0], q[1], labels[i])
+        if i==0:
+            df['Comp1_Amp']=[txt]
+        if i==1:
+            df['Comp1_Center']=[txt]
+        if i==2:
+            df['Comp1_SD']=[txt]
+        if i==3:
+            df['Comp2_Amp']=[txt]
+        if i==4:
+            df['Comp2_Center']=[txt]
+        if i==5:
+            df['Comp2_SD']=[txt]
+        if i==6:
+            df['Comp3_Amp']=[txt]
+        if i==7:
+            df['Comp3_Center']=[txt]
+        if i==8:
+            df['Comp3_SD']=[txt]
+    df.to_csv('Final Results.csv')
+if model=='1g':
+    flat_samples = sampler.get_chain(discard=100, thin=15, flat=True)
+    df=pd.DataFrame(columns=['Comp1_Amp','Comp1_Center','Comp1_SD'])
+    for i in range(ndim):
+        mcmc = np.percentile(flat_samples[:, i], [16, 50, 84])
+        q = np.diff(mcmc)
+        txt = "{0:.3f}_{{-{1:.3f}}}^{{{2:.3f}}}"
+        txt = txt.format(mcmc[1], q[0], q[1], labels[i])
+        if i==0:
+            df['Comp1_Amp']=[txt]
+        if i==1:
+            df['Comp1_Center']=[txt]
+        if i==2:
+            df['Comp1_SD']=[txt]
+    df.to_csv('Final Results.csv')
+if model=='2g':
+    flat_samples = sampler.get_chain(discard=100, thin=15, flat=True)
+    df=pd.DataFrame(columns=['Comp1_Amp','Comp1_Center','Comp1_SD','Comp2_Amp','Comp2_Center','Comp2_SD'])
+    for i in range(ndim):
+        mcmc = np.percentile(flat_samples[:, i], [16, 50, 84])
+        q = np.diff(mcmc)
+        txt = "{0:.3f}_{{-{1:.3f}}}^{{{2:.3f}}}"
+        txt = txt.format(mcmc[1], q[0], q[1], labels[i])
+        if i==0:
+            df['Comp1_Amp']=[txt]
+        if i==1:
+            df['Comp1_Center']=[txt]
+        if i==2:
+            df['Comp1_SD']=[txt]
+        if i==3:
+            df['Comp2_Amp']=[txt]
+        if i==4:
+            df['Comp2_Center']=[txt]
+        if i==5:
+            df['Comp2_SD']=[txt]
+    df.to_csv('Final Results.csv')
+if model=='l':
+    flat_samples = sampler.get_chain(discard=100, thin=15, flat=True)
+    df=pd.DataFrame(columns=['Amp','Center','Width'])
+    for i in range(ndim):
+        mcmc = np.percentile(flat_samples[:, i], [16, 50, 84])
+        q = np.diff(mcmc)
+        txt = "{0:.3f}_{{-{1:.3f}}}^{{{2:.3f}}}"
+        txt = txt.format(mcmc[1], q[0], q[1], labels[i])
+        if i==0:
+            df['Amp']=[txt]
+        if i==1:
+            df['Center']=[txt]
+        if i==2:
+            df['Width']=[txt]
+    df.to_csv('Final Results.csv')
 plotting(model)    
--- Reading_in.py
+++ Reading_in.py
@@ -7,18 +7,85 @@ from specutils.spectra import Spectrum1D, SpectralRegion
 from specutils.fitting import fit_generic_continuum
 from astropy import units as u
 import warnings
+from scipy.optimize import leastsq
+import argparse
 warnings.filterwarnings("ignore")
-
-#plt.plot(xfit,yfit)
-#plt.plot(data_new[:,0],data_new[:,1])
-filename=sys.argv[1]
-redshift=float(sys.argv[2])
-lambda_pm=int(sys.argv[5])
+parser = argparse.ArgumentParser(description='Fit spectral lines')
+parser.add_argument('filename', help='filename of spectrum that you want to fit')
+#Default values
+parser.add_argument('-z', '--redshift', default=0, type=float,
+                    help='redshift of object to be used to correct wavelengths to rest wavelengths')
+parser.add_argument('--correction', default=0,type=float,
+                    help='Dust correction term')
+parser.add_argument('--pm', default=0,type=float,
+                    help='Range from rest wavelength over which to perform fitting')
+parser.add_argument('--wavelength', default=0,type=float,
+                    help='Rest wavelength of line')
+parser.add_argument('--guess',help='Best guess file')
+parser.add_argument('--plot_limits', default=10000,type=int,
+                    help='Limit of plots')
+args = parser.parse_args()
+def model_4gauss(theta):
+    '''
+    Fit 4 Gaussian model
+    '''
+    a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12 = theta
+    model = a1*np.exp(-(x-a2)**2/(2*a3**2))+a4*np.exp(-(x-a5)**2/(2*a6**2))+a7*np.exp(-(x-a8)**2/(2*a9**2))+a10*np.exp(-(x-a11)**2/(2*a12**2))
+    return model
+def model_3gauss(theta):
+    '''
+    Fit 3 Gaussian model
+    '''
+    a1,a2,a3,a4,a5,a6,a7,a8,a9 = theta
+    model = a1*np.exp(-(x-a2)**2/(2*a3**2))+a4*np.exp(-(x-a5)**2/(2*a6**2))+a7*np.exp(-(x-a8)**2/(2*a9**2))
+    return model
+def model_2gauss(theta):
+    '''
+    Fit 2 Gaussian model
+    '''
+    a1,a2,a3,a4,a5,a6= theta
+    model = a1*np.exp(-(x-a2)**2/(2*a3**2))+a4*np.exp(-(x-a5)**2/(2*a6**2))
+    return model
+def model_1gauss(theta):
+    '''
+    Fit 1 Gaussian model
+    '''
+    a1,a2,a3 = theta
+    model = a1*np.exp(-(x-a2)**2/(2*a3**2))
+    return model
+def model_lorentzian(theta):
+    '''
+    Fit Lorentzian model
+    '''
+    a,b,c=theta
+    model=(a/np.pi)*(c/((x-b)**2+c**2))
+    return model
+def three_gaussian_fit( params ):#functions for least squares minimization
+    fit = model_3gauss( params )
+    return (fit - y)
+def two_gaussian_fit( params ):
+    fit = model_2gauss(params )
+    return (fit - y)
+def one_gaussian_fit( params ):
+    fit= model_1gauss(params )
+    return (fit - y)
+def four_gaussian_fit( params ):
+    fit = model_4gauss(params )
+    return (fit - y)
+def lorentzian_fit(params):
+    fit = model_lorentzian(params)
+    return (fit - y)
+filename=args.filename
+redshift=args.redshift
+lambda_pm=args.pm
 Rv=3.1
-Ebv=float(sys.argv[3])
+Ebv=args.correction
 ext = F99(Rv=Rv)
 def get_data(filename):
-    if filename[-5:]=='ascii' or filename[-3:]=='txt' or filename[-3:]=='dat':
+    '''
+    Read in file depending on what format it is in
+    '''
+    if filename[-3:]=='rtf' or filename[-5:]=='ascii' or filename[-3:]=='txt' or filename[-3:]=='dat':#check txt/ascii files first
         data=np.loadtxt(filename)
         wavelength=data[:,0]/(1+redshift)
         flux=data[:,1]
@@ -26,14 +93,14 @@ def get_data(filename):
         if dim>2:
             error=data[:,2]
         else:
-            error=0.05*flux
-    elif filename[-4:]=='fits':
+            error=0.2*flux
+    elif filename[-4:]=='fits':# check fits files
         f = fits.open(filename)  
-        specdata = f[0].data 
+        specdata = f[0].data #assume file is in 2nd header-could change this potentially
         wavelength=specdata[0]/(1+redshift)
         flux=specdata[1]
         error=specdata[2]
-    elif filename[-3:]=='csv':
+    elif filename[-3:]=='csv':#check csv files
         data=pd.read_csv(filename)
         wavelength=np.empty(0)
         flux=np.empty(0)
@@ -44,21 +111,93 @@ def get_data(filename):
             error=np.append(error,data.iloc[:,2][i])
     return wavelength,flux,error
 def get_wavelength_vals(filename):
-    l,f,e=get_data(filename)
-    l_ext=f/ext.extinguish(l/1e4,Ebv=Ebv)
-    line_value=int(sys.argv[4])
-    xfit=l[np.where((l>line_value-lambda_pm)&(l<line_value+lambda_pm))]
-    yfit=f[np.where((l>line_value-lambda_pm)&(l<line_value+lambda_pm))]
-    region=SpectralRegion((line_value-(lambda_pm/5))*u.nm, (line_value+(lambda_pm/5))*u.nm)
-    spectrum = Spectrum1D(flux=yfit*u.Jy, spectral_axis=xfit*u.nm)
-    g1_fit = fit_generic_continuum(spectrum,exclude_regions=region)
-    y_continuum_fitted = g1_fit(xfit*u.nm)
-    ynew=np.array(y_continuum_fitted)
+    '''
+    Read out data and convert to the desired units
+    Gives x in velocity, y in flux(in 1e-15 ergs/s/cm2/A) and yerrors in the same unit
+    '''
+    l,f,e=get_data(filename)#get lambda, flux,error from data file
+    l_ext=f/ext.extinguish(l/1e4,Ebv=Ebv)# do dust extinction
+    line_value=args.wavelength#get relevant wavelength
+    xfit=l[np.where((l>(line_value-lambda_pm))&(l<(line_value+lambda_pm)))]#get a subset of the wavelength around the line-don't fit the whole spectrum
+    yfit=f[np.where((l>(line_value-lambda_pm))&(l<(line_value+lambda_pm)))]#get a subset of the flux around the line
+    region=SpectralRegion((line_value-(lambda_pm/5))*u.nm, (line_value+(lambda_pm/5))*u.nm)# start fitting continuum with astropy
+    spectrum = Spectrum1D(flux=f*u.Jy, spectral_axis=l*u.nm)
+    g1_fit = fit_generic_continuum(spectrum,exclude_regions=region)#fit the whole spectrum-but exclude the relevant emission line
+    y_continuum_fitted = g1_fit(l*u.nm)
+    ynew=np.array(y_continuum_fitted)[np.where((l>line_value-lambda_pm)&(l<line_value+lambda_pm))]# now filter the continuum solution to relevant values
     y=yfit-ynew
-    x=3e5*(xfit-line_value)/(line_value)
     yerr=e[np.where((l>line_value-lambda_pm)&(l<line_value+lambda_pm))]   
+    if abs(np.mean(y))<1e-10:
+        y*=1e15
+        yerr*=1e15
+    if abs(np.median(y))>0.01:
+        y-=np.median(y)
+    x=3e5*(xfit-line_value)/(line_value)
     return x,y,yerr    
 x,y,e=get_wavelength_vals(filename)
-plt.plot(x,y)
-plt.show()
+if args.guess is None:
+    fit1 = leastsq( one_gaussian_fit, [0.1,10,100] )
+    fit2 = leastsq( two_gaussian_fit, [0.1,10,100,0.1,10,100] )
+    fit3 = leastsq( three_gaussian_fit, [0.1,10,100,0.1,10,100,0.1,10,100] )
+    fit4 = leastsq( four_gaussian_fit, [0.1,10,1000,0.1,10,500,0.1,10,100,0.1,1000,100] )
+    fit5 = leastsq( lorentzian_fit, [0.1,10,1000] )
+    chi1=1/(len(y)-len(fit1[0]))*sum((y-model_1gauss(fit1[0]))**2/e**2)
+    chi2=1/(len(y)-len(fit2[0]))*sum((y-model_2gauss(fit2[0]))**2/e*2)
+    chi3=1/(len(y)-len(fit3[0]))*sum((y-model_3gauss(fit3[0]))**2/e**2)
+    chi4=1/(len(y)-len(fit4[0]))*sum((y-model_4gauss(fit4[0]))**2/e**2)
+    chi5=1/(len(y)-len(fit5[0]))*sum((y-model_lorentzian(fit5[0]))**2/e**2)
+    chi_array=np.array([chi1,chi2,chi3,chi4,chi5])
+    min_chisq=min(chi_array)
+    print('Minimum chi^2 with rough guess', min_chisq)
+    if np.argmin(chi_array)==0:
+        print("Our initial least squares guess is a 1 component gaussian with parameters:",fit1[0])
+        plt.plot(x,model_1gauss(fit1[0]),color='r')
+    elif np.argmin(chi_array)==1:
+        print("Our initial least squares guess is a 2-component gaussian with parameters :",fit2[0])
+        plt.plot(x,model_2gauss(fit2[0]),color='r')
+    elif np.argmin(chi_array)==2:
+        print("Our initial least squares guess is a 3 component gaussian with parameters:",fit3[0])
+        plt.plot(x,model_3gauss(fit3[0]),color='r')
+    elif np.argmin(chi_array)==3:
+        print("Our initial least squares guess is a 4 component gaussian with parameters:",fit4[0])
+        plt.plot(x,model_4gauss(fit4[0]),color='r')
+    elif np.argmin(chi_array)==4:
+        print(fit5[0])
+        plt.plot(x,model_lorentizan(fit5[0]),color='r')
+    plt.plot(x,y,alpha=0.6)
+    plt.show()
+elif args.guess is not None:
+    guess=np.loadtxt(args.guess)
+    if len(guess)==6:
+        fit = leastsq( two_gaussian_fit, guess)
+        print("Final leastsq values=",fit[0])
+        plt.plot(x,model_2gauss(fit[0]),color='blue')
+        plt.plot(x,model_1gauss(fit[0][0:3]),color='r')
+        plt.plot(x,model_1gauss(fit[0][3:6]),color='r')
+    elif len(guess)==9:
+        fit = leastsq( three_gaussian_fit, guess)
+        print("Final leastsq values=",fit[0])
+        plt.plot(x,model_3gauss(fit[0]),color='blue')
+        plt.plot(x,model_1gauss(fit[0][0:3]),color='r')
+        plt.plot(x,model_1gauss(fit[0][3:6]),color='r')
+        plt.plot(x,model_1gauss(fit[0][6:9]),color='r')
+    elif len(guess)==12:
+        fit = leastsq( four_gaussian_fit, guess)
+        print("Final leastsq values=",fit[0])
+        plt.plot(x,model_4gauss(fit[0]),color='blue')
+        plt.plot(x,model_1gauss(fit[0][0:3]),color='r')
+        plt.plot(x,model_1gauss(fit[0][3:6]),color='r')
+        plt.plot(x,model_1gauss(fit[0][6:9]),color='r')
+        plt.plot(x,model_1gauss(fit[0][9:12]),color='r')
+    elif len(guess)==3 and sys.argv[7]!='L':
+        fit = leastsq( one_gaussian_fit, guess)
+        print("Final leastsq values=",fit[0])
+        plt.plot(x,model_1gauss(fit[0]),color='r')
+    elif len(guess)==3 and sys.argv[7]=='L':
+        fit = leastsq(lorentzian_fit, guess)
+        print("Final leastsq values=",fit[0])
+        plt.plot(x,model_lorentzian(fit[0]),color='r')
+    plt.plot(x,y,alpha=0.6,color='black')
+    plt.xlim(-args.plot_limits,args.plot_limits)
+    plt.show()
 
